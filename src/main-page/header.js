0b63fd104f98855d3f7f83c345d94f276d2d4e59
peaceapple <astonreba@gmail.com>
2023-08-23 13:46:21 +0300

    Look at class component alternative

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -1,5 +1,23 @@
+// import { Component } from 'react';
 import logo from './GloboLogo.png';
 
+//class component can also be used; requires use of this keyword with props;
+// class Header extends Component {
+//     render() {
+//         return(
+//             <header className='row'>
+//                 <div className='col-md-5'>
+//                     <img src={logo} className='logo' alt='logo' />
+//                 </div>
+//                 <div className='col-md-7 mt-5 subtitle'>
+//                 {this.props.subtitle}
+//                 </div>
+//             </header>
+//         );
+//     }
+// }
+
+//function component
 const Header = ({subtitle}) => (
     <header className='row'>
         <div className='col-md-5'>
