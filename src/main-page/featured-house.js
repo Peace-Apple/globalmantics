bf92af98b18e6c33b61a53c31db17b316c97c102
peaceapple <astonreba@gmail.com>
2023-08-24 13:52:50 +0300

    Add featured house component

---
+++ src/main-page/featured-house.js
@@ -0,0 +1,13 @@
+const FeaturedHouse = ({house}) => {
+    if (house)
+        return (  
+            <div>
+                <div className="row featuredHouse">
+                    <h3 className="col-md-12 text-center">Featured House</h3>
+                </div> 
+            </div>
+        );
+    return <div>No featured house at this time</div>;
+}
+ 
+export default FeaturedHouse;
