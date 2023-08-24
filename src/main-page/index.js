233ac0d7eab12b88823dee22173391ded1c0cbaf
peaceapple <astonreba@gmail.com>
2023-08-24 13:01:01 +0300

    Add hook to fetch houses

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import './main-page.css';
 import Header from './header';
 
 function App() {
+  useEffect(()=>{
+    const fetchHouses = async () => {
+      const rsp = await fetch('/houses.json');
+      const houses = await rsp.json();
+    }
+    fetchHouses();
+  }, [])
   return (
     <div className="container">
       <Header subtitle='Providing houses all over the world' />
